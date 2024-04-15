8dba3b684c547854e982bed9be9c52f0d70fcb42
Khoa Le <kle@screenbeam.com>
2024-04-14 23:29:55 -0700

    do not use port if not defined in .env

--- src/lib/api/http.js
+++ src/lib/api/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PORT = import.meta.env.VITE_API_PORT || 8001;
+const PORT = import.meta.env.VITE_API_PORT || '';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost';
 
 const http = axios.create({
