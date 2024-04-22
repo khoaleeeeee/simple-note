c0be3d29ca8a83b5d53c9f78ddfec91b34433be8
Khoa Le <kle@screenbeam.com>
2024-04-21 23:04:09 -0700

    attached port to only localhost

--- src/lib/api/http.js
+++ src/lib/api/http.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const PORT = import.meta.env.VITE_API_PORT || '';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || `http://localhost:${PORT}`;
 
 const http = axios.create({
-	baseURL: `${API_BASE_URL}:${PORT}`
+	baseURL: `${API_BASE_URL}`
 });
 
 export default http;
