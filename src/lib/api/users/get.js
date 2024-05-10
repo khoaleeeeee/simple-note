211e71a7535b66a522fba3708fa3e6a8eb2e145e
Khoa Le <kle@screenbeam.com>
2024-05-09 19:02:03 -0700

    use local database for dev

--- src/lib/api/users/get.js
+++ src/lib/api/users/get.js
@@ -1,6 +1,16 @@
 import http from '../http';
 
 const get = async () => {
+	const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+	if (API_BASE_URL.includes('localhost')) {
+		return {
+			uuid: '4d132e39-da34-4150-99fc-d3305d1ebdbc',
+			name: 'Khoa Dep Trai',
+			email: 'khoaleeeeee@gmail.com'
+		};
+	}
+
 	try {
 		const resp = await http.get('/api/users');
 
