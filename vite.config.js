ea8f007abadf84046c12db280cc3542715e8bcd7
Khoa Le <kle@screenbeam.com>
2024-08-11 21:23:33 -0700

    imported crypto from rollup

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-
 	server: {
 		host: true,
 		port: 8000
+	},
+	build: {
+		rollupOptions: {
+			external: ['crypto-js']
+		}
 	}
-	// proxy: {
-	// 	'/api': {
-	// 		target: 'http://localhost:8001'
-	// 	}
-	// }
 };
 
 export default config;
